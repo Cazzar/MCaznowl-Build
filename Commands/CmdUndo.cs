cc4869279874da8bf409f88e55b10d7686a3255a
Colin O'Dell <colinodell@gmail.com>
2011-01-07 12:04:33 -0500

    /undo will now work if used by console.  /undo physics may still have issues (untested).

--- Commands/CmdUndo.cs
+++ Commands/CmdUndo.cs
@@ -22,7 +22,7 @@ namespace MCForge
 
             if (message.Split(' ').Length == 2)
             {
-                if (message.Split(' ')[1].ToLower() == "all" && p.group.Permission > LevelPermission.Operator)
+                if (message.Split(' ')[1].ToLower() == "all" && p != null && p.group.Permission > LevelPermission.Operator)
                 {
                     seconds = 500000;
                 }
