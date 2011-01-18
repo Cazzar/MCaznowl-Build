03664f0cc57c007f1e47776cc9fbe9daa5353e78
Colin O'Dell <colinodell@gmail.com>
2011-01-18 18:30:13 -0500

    Added a checker to /zz to see if the players rank can use /cuboid or if they can use /static; if they cant use one of them they cant use /zz!

--- Commands/CmdZz.cs
+++ Commands/CmdZz.cs
@@ -6,14 +6,28 @@ namespace MCForge
 	{
                 public override string name { get { return "zz"; } }
 		public override string shortcut { get { return ""; } }
-		public override string type { get { return "other"; } }
+        public override string type { get { return "mod"; } }
 		public override bool museumUsable { get { return false; } }
 		public override LevelPermission defaultRank { get { return LevelPermission.Builder; } }
 		public override void Use(Player p, string message)
 		{
-			Command.all.Find("static").Use(p, message);
-                        Command.all.Find("cuboid").Use(p, message);
-                        Player.SendMessage(p, p.color + p.name + Server.DefaultColor + " to stop this, use /abort");
+            if ((p.group.CanExecute(Command.all.Find("cuboid"))) && (p.group.CanExecute(Command.all.Find("static"))))
+            {
+                if ((!p.staticCommands == true) && (!p.megaBoid == true))
+                {
+                    Command.all.Find("static").Use(p, message);
+                    Command.all.Find("cuboid").Use(p, message);
+                    Player.SendMessage(p, p.color + p.name + Server.DefaultColor + " to stop this, use /zz again");
+                }
+                else 
+                {
+                    p.ClearBlockchange();
+                    p.staticCommands = false;
+                    Player.SendMessage(p, "/zz has ended.");
+                }
+            }
+            else { Player.SendMessage(p, "Sorry, your rank cannot use one of the commands this uses!"); }
+               
 		}
 		public override void Help(Player p)
 		{
