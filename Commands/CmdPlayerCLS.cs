7ce550ebc8369122813d6586c164d430b127f7d2
Colin O'Dell <colinodell@gmail.com>
2011-04-22 11:08:57 -0400

    Added /playercls and /globalcls (aliases are: /cls and /gcls)

---
+++ Commands/CmdPlayerCLS.cs
@@ -0,0 +1,45 @@
+/*
+	Copyright 2011 MCForge
+		
+	Dual-licensed under the	Educational Community License, Version 2.0 and
+	the GNU General Public License, Version 3 (the "Licenses"); you may
+	not use this file except in compliance with the Licenses. You may
+	obtain a copy of the Licenses at
+	
+	http://www.osedu.org/licenses/ECL-2.0
+	http://www.gnu.org/licenses/gpl-3.0.html
+	
+	Unless required by applicable law or agreed to in writing,
+	software distributed under the Licenses are distributed on an "AS IS"
+	BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+	or implied. See the Licenses for the specific language governing
+	permissions and limitations under the Licenses.
+*/
+using System;
+
+namespace MCForge
+{
+    public class CmdPlayercls : Command
+    {
+        public override string name { get { return "playercls"; } }
+        public override string shortcut { get { return "cls"; } }
+        public override string type { get { return "other"; } }
+        public override bool museumUsable { get { return true; } }
+        public override LevelPermission defaultRank { get { return LevelPermission.Guest; } }
+
+        public override void Use(Player p, string message)
+        {
+           int i = 0;
+           for (i = 0; i < 20; i++)
+           {
+               Player.SendMessage(p, ".");
+           }
+           Player.SendMessage(p, "%4Chat cleared.");
+        }
+
+        public override void Help(Player p)
+        {
+            Player.SendMessage(p, "/playercls - Clears your chat.");
+        }
+    }
+}
