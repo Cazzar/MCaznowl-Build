0ccdb174a2eda28ef64f0a3a3b89445a067a8222
Aaron Lawson <cripplecowboycowoutfit@yahoo.com>
2011-06-30 12:19:48 -0700

    Updated Licensing Information.

--- Commands/CmdTopTen.cs
+++ Commands/CmdTopTen.cs
@@ -1,16 +1,16 @@
-﻿/* 
-	Copyright 2011 MCDerp Team Based in Canada
-	MCDerp has been Licensed with the below license:
-	
-	http://www.binpress.com/license/view/l/62e7c4034ccb45cd39d8dcbe9ed87bd8
-	Or, you can read the below summary;
-
-	Can be used on 1 site, unlimited servers
-	Personal use only (cannot be resold or distributed)
-	Non-commercial use only
-	Cannot modify source-code for any purpose (cannot create derivative works)
-	Software trademarks are included in the license
-	Software patents are included in the license
+/*
+Copyright 2011 MCForge
+Dual-licensed under the Educational Community License, Version 2.0 and
+the GNU General Public License, Version 3 (the "Licenses"); you may
+not use this file except in compliance with the Licenses. You may
+obtain a copy of the Licenses at
+http://www.osedu.org/licenses/ECL-2.0
+http://www.gnu.org/licenses/gpl-3.0.html
+Unless required by applicable law or agreed to in writing,
+software distributed under the Licenses are distributed on an "AS IS"
+BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied. See the Licenses for the specific language governing
+permissions and limitations under the Licenses.
 */
 		
 using System;
