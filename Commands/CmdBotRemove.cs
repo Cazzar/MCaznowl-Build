c696878cd59ad5d2204df9a896a775a19c3339e8
Jack1312 <mczombie@live.com.au>
2011-07-01 21:11:21 -0700

    /botremove - Added null checker

--- Commands/CmdBotRemove.cs
+++ Commands/CmdBotRemove.cs
@@ -33,32 +33,38 @@ namespace MCForge
         public override void Use(Player p, string message)
         {
             if (message == "") { Help(p); return; }
-            try
+            if (p == null)
             {
-                if (message.ToLower() == "all")
+                Player.SendMessage(p, "This command can only be used in-game!");
+                return;
+            }
+                try
                 {
-                    for (int i = 0; i < PlayerBot.playerbots.Count; i++)
+                    if (message.ToLower() == "all")
                     {
-                        if (PlayerBot.playerbots[i].level == p.level)
+                        for (int i = 0; i < PlayerBot.playerbots.Count; i++)
                         {
-                            //   PlayerBot.playerbots.Remove(PlayerBot.playerbots[i]);
-                            PlayerBot Pb = PlayerBot.playerbots[i];
-                            Pb.removeBot();
-                            i--;
+                            if (PlayerBot.playerbots[i].level == p.level)
+                            {
+                                //   PlayerBot.playerbots.Remove(PlayerBot.playerbots[i]);
+                                PlayerBot Pb = PlayerBot.playerbots[i];
+                                Pb.removeBot();
+                                i--;
+                            }
                         }
                     }
+                    else
+                    {
+                        PlayerBot who = PlayerBot.Find(message);
+                        if (who == null) { Player.SendMessage(p, "There is no bot " + who + "!"); return; }
+                        if (p.level != who.level) { Player.SendMessage(p, who.name + " is in a different level."); return; }
+                        who.removeBot();
+                        Player.SendMessage(p, "Removed bot.");
+                    }
                 }
-                else
-                {
-                    PlayerBot who = PlayerBot.Find(message);
-                    if (who == null) { Player.SendMessage(p, "There is no bot " + who + "!"); return; }
-                    if (p.level != who.level) { Player.SendMessage(p, who.name + " is in a different level."); return; }
-                    who.removeBot();
-                    Player.SendMessage(p, "Removed bot.");
-                }
+                catch (Exception e) { Server.ErrorLog(e); Player.SendMessage(p, "Error caught"); }
             }
-            catch (Exception e) { Server.ErrorLog(e); Player.SendMessage(p, "Error caught"); }
-        }
+        
         public override void Help(Player p)
         {
             Player.SendMessage(p, "/botremove <name> - Remove a bot on the same level as you");
