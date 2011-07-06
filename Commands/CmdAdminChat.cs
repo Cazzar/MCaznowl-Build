781848f51baa41368c63dcd34c0c064ea32446bf
Aaron Lawson <cripplecowboycowoutfit@yahoo.com>
2011-07-06 16:03:47 -0700

    Fixed License Header in CmdAdminChat.cs

--- Commands/CmdAdminChat.cs
+++ Commands/CmdAdminChat.cs
@@ -1,18 +1,16 @@
-﻿/* 
-	Copyright 2011 MCDerp Team Based in Canada
-	MCDerp has been Licensed with the below license:
-	
-	http://www.binpress.com/license/view/l/62e7c4034ccb45cd39d8dcbe9ed87bd8
-	Or, you can read the below summary;
-
-	Can be used on 1 site, unlimited servers
-	Personal use only (cannot be resold or distributed)
-	Non-commercial use only
-	Cannot modify source-code for any purpose (cannot create derivative works)
-	Software trademarks are included in the license
-	Software patents are included in the license
-*/
-		
+/*
+Copyright 2011 MCForge
+Licensed under the
+Educational Community License, Version 2.0 (the "License"); you may
+not use this file except in compliance with the License. You may
+obtain a copy of the License at
+http://www.opensource.org/licenses/ecl2.php
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an "AS IS"
+BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied. See the License for the specific language governing
+permissions and limitations under the License.
+*/	
 
 using System;
 
