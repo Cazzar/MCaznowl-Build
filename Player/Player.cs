a6cd2737a83923c289a242fa3fbf8375cb8e6f74
Eddie Penta <hypereddie10@gamezgalaxy.com>
2011-07-29 23:47:03 -0300

    || has to be &&

--- Player/Player.cs
+++ Player/Player.cs
@@ -300,7 +300,7 @@ namespace MCForge
                 socket.BeginReceive(tempbuffer, 0, tempbuffer.Length, SocketFlags.None, new AsyncCallback(Receive), this);
                 timespent.Elapsed += delegate
                 {
-                    if (!Loading || !Server.afkset.Contains(name))
+                    if (!Loading && !Server.afkset.Contains(name))
                     {
                         try
                         {
