20b3afaaf32beb67029904cec5414a1fa000f0c7
Aaron Lawson <cripplecowboycowoutfit@yahoo.com>
2011-08-13 15:22:12 -0500

    Fixed an issue in /resetpass where, if the person you specified for a password reset didn't exist, it would cause an error.

--- Commands/CmdResetPass.cs
+++ Commands/CmdResetPass.cs
@@ -31,20 +31,31 @@ namespace MCForge
         public override string shortcut { get { return ""; } }
         public override string type { get { return "other"; } }
         public override bool museumUsable { get { return true; } }
-        public override LevelPermission defaultRank { get { return LevelPermission.Banned; } }
+        public override LevelPermission defaultRank { get { return LevelPermission.Admin; } }
         public CmdResetPass() { }
 
         public override void Use(Player p, string message)
         {
+            if (message == "") { Help(p); return; }
             Player who = Player.Find(message);
-            if (Server.server_owner == "Notch" && Server.server_owner != p.name)
+            if (Server.server_owner == "Notch" || Server.server_owner == "")
             {
-                Player.SendMessage(p, "Your not the server owner!");
+                Player.SendMessage(p, "Please tell the server owner to change the 'Server Owner' property.");
                 return;
             }
-            if (p.adminpen == true)
+            if (p != null && Server.server_owner != p.name)
+            {
+                Player.SendMessage(p, "You're not the server owner!");
+                return;
+            }
+            if (p != null && p.adminpen == true)
             {
                 Player.SendMessage(p, "You cannot reset a password while in the admin pen!");
+                return;
+            }
+            if (who == null)
+            {
+                Player.SendMessage(p, "The specified player does not exist.");
                 return;
             }
             if (!File.Exists("extra/passwords/" + who.name + ".xml"))
@@ -52,8 +63,16 @@ namespace MCForge
                 Player.SendMessage(p, "The player you specified does not have a password!");
                 return;
             }
-            File.Delete("extra/passwords/" + who.name + ".xml");
-            Player.SendMessage(p, "The admin password has sucessfully been removed for " + who.color + who.name + "!");
+            try
+            {
+                File.Delete("extra/passwords/" + who.name + ".xml");
+                Player.SendMessage(p, "The admin password has sucessfully been removed for " + who.color + who.name + "!");
+            }
+            catch (Exception e)
+            {
+                Player.SendMessage(p, "Password Deletion Failed. Please manually delete the file. It is in extra/passwords.");
+                Server.ErrorLog(e);
+            }
         }
         public override void Help(Player p)
         {
