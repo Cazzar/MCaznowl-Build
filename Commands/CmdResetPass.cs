34ce9aff8e60e508bcd95db3c86393993a78221b
Jack13122 <jack.h13@hotmail.com>
2011-08-13 16:49:37 +1000

    Added it.

--- Commands/CmdResetPass.cs
+++ Commands/CmdResetPass.cs
@@ -1,4 +1,7 @@
 /*
+
+        Written by Jack1312
+
 	Copyright 2011 MCForge
 		
 	Dual-licensed under the	Educational Community License, Version 2.0 and
@@ -14,6 +17,7 @@
 	BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 	or implied. See the Licenses for the specific language governing
 	permissions and limitations under the Licenses.
+
 */
 using System;
 using System.Collections.Generic;
