b165323f77c7bfa8101f8755e3e04ba6d703f793
Aaron Lawson <cripplecowboycowoutfit@yahoo.com>
2011-08-21 21:30:39 -0500

    Updated Dev list and version #

--- Server.cs
+++ Server.cs
@@ -96,7 +96,7 @@ public static event OnServerError ServerError = null;
         public static PlayerList muted;
         public static PlayerList ignored;
 
-        public static List<string> devs = new List<string>(new string[] { "dmitchell94", "jordanneil23", "501st_commander", "fenderrock87", "edh649", "philipdenseje", "hypereddie10", "uberfox", "erickilla", "the_legacy", "fredlllll", "jakenator14", "jack1312", "soccer101nic", "headdetect", "merlin33069", "bizarrecake", "jasonbay13", "cazzar", "snowl"});
+        public static List<string> devs = new List<string>(new string[] { "dmitchell94", "jordanneil23", "501st_commander", "fenderrock87", "edh649", "philipdenseje", "hypereddie10", "uberfox", "erickilla", "the_legacy", "fredlllll", "jakenator14", "jack1312", "soccer101nic", "headdetect", "merlin33069", "bizarrecake", "jasonbay13", "cazzar", "snowl", "techjar"});
 
         public static List<TempBan> tempBans = new List<TempBan>();
         public struct TempBan { public string name; public DateTime allowedJoin; }
