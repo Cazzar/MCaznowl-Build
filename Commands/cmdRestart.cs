bc9e1066ef241e98632202bac4a73b7e1c0a6ff2
hypereddie10 <eddie@gamezgalaxy.com>
2011-08-22 23:52:42 -0400

    cmdrestart now saves and disposes before restarting..

--- Commands/cmdRestart.cs
+++ Commands/cmdRestart.cs
@@ -36,6 +36,7 @@ namespace MCForge
         public override void Use(Player p, string message)
         {
             if (message != "") { Help(p); return; }
+            Server.Exit();
 			using (Process Restarter = new Process())
 			{
 				Restarter.StartInfo.FileName = "Restarter.exe";
