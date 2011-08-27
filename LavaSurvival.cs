1f9692c4cfa1feeb649017873ba8f3210b754744
Techjar <tecknojar@gmail.com>
2011-08-26 23:44:04 -0400

    Added license text to class files and added another class for Lava Survival.

--- LavaSurvival.cs
+++ LavaSurvival.cs
@@ -17,25 +17,13 @@
 	permissions and limitations under the Licenses.
 */
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace MCForge
 {
-    class CmdLavaSurvival : Command
+    class LavaSurvival
     {
-        public override string name { get { return "lavasurvival"; } }
-        public override string shortcut { get { return "ls"; } }
-        public override string type { get { return "other"; } }
-        public override bool museumUsable { get { return false; } }
-        public override LevelPermission defaultRank { get { return LevelPermission.Operator; } }
-        public CmdLavaSurvival() { }
-
-        public override void Use(Player p, string message)
-        {
-            
-        }
-        public override void Help(Player p)
-        {
-            Player.SendMessage(p, "/lavasurvival - Various commands to setup Lava Survival.");
-        }
     }
 }
