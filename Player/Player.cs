ede5cff8be7d9598adac66afdf63ee4bd87bddc6
jasonbay13 <jasonbay13@yahoo.com>
2011-09-04 10:04:15 -0400

    Player position update should no longer send 9 bytes to each player when poition is unchanged.

--- Player/Player.cs
+++ Player/Player.cs
@@ -2677,11 +2677,14 @@ namespace MCForge
             {
                 changed |= 2;
             }
-            if (Math.Abs(pos[0] - basepos[0]) > 32 || Math.Abs(pos[1] - basepos[1]) > 32 || Math.Abs(pos[2] - basepos[2]) > 32)
+            /*if (Math.Abs(pos[0] - basepos[0]) > 32 || Math.Abs(pos[1] - basepos[1]) > 32 || Math.Abs(pos[2] - basepos[2]) > 32)
                 changed |= 4;
 
             if ((oldpos[0] == pos[0] && oldpos[1] == pos[1] && oldpos[2] == pos[2]) && (basepos[0] != pos[0] || basepos[1] != pos[1] || basepos[2] != pos[2]))
+                changed |= 4;*/
+            if (Math.Abs(pos[0] - oldpos[0]) > 32 || Math.Abs(pos[1] - oldpos[1]) > 32 || Math.Abs(pos[2] - oldpos[2]) > 32)
                 changed |= 4;
+            if (changed == 0) { if (oldpos[0] != pos[0] || oldpos[1] != pos[1] || oldpos[2] != pos[2]) changed |= 1; }
 
             byte[] buffer = new byte[0]; byte msg = 0;
             if ((changed & 4) != 0)
