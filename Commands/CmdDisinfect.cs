72eb7f6f1b181ec57fa73626522b2bf7a72cbd72
Snowl <david-.-b@live.com>
2011-12-05 09:04:37 +1100

    Fixed Disinfect

--- Commands/CmdDisinfect.cs
+++ Commands/CmdDisinfect.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 
 namespace MCForge
@@ -23,12 +23,8 @@ namespace MCForge
             {
                 if (!who.referee)
                 {
-                    int leftMinute = (int)((Server.zombie.amountOfMilliseconds / (1000 * 60)) % 60);
-                    if (leftMinute > 1)
-                    {
-                        Server.zombie.DisinfectPlayer(who);
-                        Player.GlobalMessage(p.color + p.name + Server.DefaultColor + " just got Disnfected!");
-                    }
+                    Server.zombie.DisinfectPlayer(who);
+                    Player.GlobalMessage(p.color + p.name + Server.DefaultColor + " just got Disnfected!");
                 }
             }
         }
