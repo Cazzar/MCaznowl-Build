190dc8d4080bca820f2c02111ef08fe386431e06
Techjar <tecknojar@gmail.com>
2011-12-15 17:20:25 -0500

    Sleeping for 20 milliseconds is way more effective than ONE MILLISECOND.

--- Commands/CmdFly.cs
+++ Commands/CmdFly.cs
@@ -51,7 +51,7 @@ namespace MCForge
                 List<Pos> buffer = new List<Pos>();
                 while (p.isFlying)
                 {
-                    Thread.Sleep(1);
+                    Thread.Sleep(20);
                     if (p.pos == oldpos) continue;
                     try
                     {
